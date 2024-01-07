c11acff3cc998051029d8ae48995be9f2c63528b
Stefania03 <stefania-miruna.tudor@student.unitbv.ro>
2024-01-07 16:02:43 +0200

    Minor fixes

--- Expresie Regulata -- AFD/main.cpp
+++ Expresie Regulata -- AFD/main.cpp
@@ -7,20 +7,21 @@
 
 int main()
 {
+	DeterministicFiniteAutomaton automaton;
+
 	std::ifstream input("input.txt");
 	ExpressionToAutomaton expressionToAutomaton(input);
 	if (expressionToAutomaton.IsExpressionValid())
 	{
 		std::cout << "The expression is valid" << std::endl;
 		expressionToAutomaton.SetPolishForm();
-		expressionToAutomaton.CreateAutomatonFromPolishForm();
+		automaton = expressionToAutomaton.CreateAutomatonFromPolishForm();
 	}
 	else
 	{
 		std::cout << "The expression is not valid" << std::endl;
 	}
 
-	DeterministicFiniteAutomaton automaton;
 	std::cout << automaton;
 	return 0;
 }
